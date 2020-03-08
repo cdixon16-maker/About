58697a2edf6a58a50d7b5133f6f193f7b5563756
Chase Dixon <schasedixon16@gmail.com>
2020-03-07 20:34:36 -0700

    Video

--- src/contactme.js
+++ src/contactme.js
@@ -6,16 +6,17 @@ function contactme()
 { return (
     <div className="contact">
     <div className="me">
-   <h5 >Contact Me</h5>
-   <p>Chase Dixon</p>
-   <p>Email: schasedixon16@gmail.com</p>
-   <p>Phone: 801-864-9875</p>
-   <a href="https://www.linkedin.com/in/s-chase-dixon/" class="btn btn-outline-primary btn-md" role="button"  target="_blank" rel="noopener noreferrer">in</a>
-   <a href="https://github.com/cdixon16-maker" class="btn btn-outline-light btn-md" role="button"  target="_blank" rel="noopener noreferrer">GitHub</a>
-   </div>
+        <h5 >Contact Me</h5>
+        <p>Chase Dixon</p>
+        <p>Email: schasedixon16@gmail.com</p>
+         <p>Phone: 801-864-9875</p>
+        <a href="https://www.linkedin.com/in/s-chase-dixon/" class="btn btn-outline-primary btn-md" role="button"  target="_blank" rel="noopener noreferrer">in</a>
+        <a href="https://github.com/cdixon16-maker" class="btn btn-outline-light btn-md" role="button"  target="_blank" rel="noopener noreferrer">GitHub</a>
+    </div>
 
-   <div class="youtube embed">
-   <iframe width="800" height="450" src="https://www.youtube.com/embed/3sK3wJAxGfs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="video"></iframe>
+    <div class="videoWrapper">
+        <iframe width="600" height="400" src="https://www.youtube.com/embed/3sK3wJAxGfs" frameborder="0" allowfullscreen title="Motivational Video">
+        </iframe>
     </div>
 
    </div>
